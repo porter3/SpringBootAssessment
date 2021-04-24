27d06dc7734782f9624c2b28150675649c4877c7
Jake Porter <jakeporter310@gmail.com>
2021-04-24 13:15:43 -0400

    add controller method shells

--- springbootassessment/src/main/java/com/jacobporter/springbootassessment/controllers/PlayerController.java
+++ springbootassessment/src/main/java/com/jacobporter/springbootassessment/controllers/PlayerController.java
@@ -1,9 +1,38 @@
 package com.jacobporter.springbootassessment.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.jacobporter.springbootassessment.domainEntities.Player;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("player")
 public class PlayerController {
+
+    @GetMapping("/get")
+    public ResponseEntity<Player> getPlayer() {
+
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Player> addPlayer() {
+
+    }
+
+    @PatchMapping("/update")
+    public ResponseEntity<Player> updatePlayer() {
+
+    }
+
+    @DeleteMapping("/delete")
+    public HttpStatus deletePlayer() {
+
+    }
+
+    @GetMapping("/search")
+    public List<Player> searchPlayers() {
+
+    }
 }
