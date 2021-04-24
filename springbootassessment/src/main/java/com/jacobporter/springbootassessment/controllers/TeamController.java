ce9dafe04834bd4967086338ade9ba48bdd294f4
Jake Porter <jakeporter310@gmail.com>
2021-04-23 22:04:54 -0400

    add controller classes for exception handling, player and team REST services

---
+++ springbootassessment/src/main/java/com/jacobporter/springbootassessment/controllers/TeamController.java
@@ -0,0 +1,9 @@
+package com.jacobporter.springbootassessment.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("team")
+public class TeamController {
+}
