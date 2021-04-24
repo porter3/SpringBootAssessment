5e1febde283d39322c8e435dee947cd78ac59e79
Jake Porter <jakeporter310@gmail.com>
2021-04-23 21:48:05 -0400

    add project structure, including dependencies and main class

---
+++ springbootassessment/src/test/java/com/jacobporter/springbootassessment/SpringbootassessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacobporter.springbootassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootassessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
